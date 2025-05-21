a5291bb6ce719e27b06aa91117d58126828d37cb
aocastro <adriano.castro2@etec.sp.gov.br>
2025-05-21 11:20:37 -0300

    Desenvolvimetno da tela de login

---
+++ login.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SESSION PHP</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+    <div class="container">
+        <div class="row justify-content-center login">
+            <div class="col-12 col-sm-6 col-md-4">
+                <div class="card shadow">
+                    <div class="card-header bg-info">
+                        <h4 class="text-center text-dark">
+                            Login do sistema
+                        </h4>
+                    </div>
+                    <div class="card-body">
+                        <form action="" method="post">
+                            <div class="form-group">
+                                <label>Login</label>
+                                <input type="text" name="login" id="login" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label>Password</label>
+                                <input type="password" name="password" id="password" class="form-control">
+                            </div>
+                            <div class="form-group row justify-content-end">
+                                <div class="col-12 col-sm-8 col-md-6">
+                                    <button type="submit" class="btn btn-outline-primary btn-block">Acessar</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="card-footer bg-info">
+                        <p class="text-center text-dark">
+                            Todos os direitos reservados.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+</body>
+</html>
